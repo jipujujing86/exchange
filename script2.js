4af56763370315f21a6ac44e3dd4265e6c9e5800
n_kikui <jipujujing86@gmail.com>
2021-02-04 12:15:17 +0900

    imageの参照パスを修正

---
+++ script2.js
@@ -0,0 +1,212 @@
+'use strict';
+/*jshint esversion:6, node:true*/
+/*globals window,console,document*/
+const name = document.querySelector('html').id;
+
+switch (name) {
+    case 'DEF':
+        document.querySelector('option[value="index.html"]').selected = true;
+        break;
+    case 'ISL':
+        document.querySelector('option[value="index_01.html"]').selected = true;
+        break;
+    case 'IRL':
+        document.querySelector('option[value="index_02.html"]').selected = true;
+        break;
+    case 'USA':
+        document.querySelector('option[value="index_03.html"]').selected = true;
+        break;
+    case 'ARE':
+        document.querySelector('option[value="index_04.html"]').selected = true;
+        break;
+    case 'ISR':
+        document.querySelector('option[value="index_05.html"]').selected = true;
+        break;
+    case 'IRN':
+        document.querySelector('option[value="index_06.html"]').selected = true;
+        break;
+    case 'IND':
+        document.querySelector('option[value="index_07.html"]').selected = true;
+        break;
+    case 'GBR':
+        document.querySelector('option[value="index_08.html"]').selected = true;
+        break;
+    case 'EGY':
+        document.querySelector('option[value="index_09.html"]').selected = true;
+        break;
+    case 'EST':
+        document.querySelector('option[value="index_10.html"]').selected = true;
+        break;
+    case 'ETH':
+        document.querySelector('option[value="index_11.html"]').selected = true;
+        break;
+    case 'AUT':
+        document.querySelector('option[value="index_12.html"]').selected = true;
+        break;
+    case 'NLD':
+        document.querySelector('option[value="index_13.html"]').selected = true;
+        break;
+    case 'GHA':
+        document.querySelector('option[value="index_14.html"]').selected = true;
+        break;
+    case 'GIN':
+        document.querySelector('option[value="index_15.html"]').selected = true;
+        break;
+    case 'GRC':
+        document.querySelector('option[value="index_16.html"]').selected = true;
+        break;
+    case 'KGZ':
+        document.querySelector('option[value="index_17.html"]').selected = true;
+        break;
+    case 'GTM':
+        document.querySelector('option[value="index_18.html"]').selected = true;
+        break;
+    case 'GRL':
+        document.querySelector('option[value="index_19.html"]').selected = true;
+        break;
+    case 'KEN':
+        document.querySelector('option[value="index_20.html"]').selected = true;
+        break;
+    case 'COL':
+        document.querySelector('option[value="index_21.html"]').selected = true;
+        break;
+    case 'SAU':
+        document.querySelector('option[value="index_22.html"]').selected = true;
+        break;
+    case 'JAM':
+        document.querySelector('option[value="index_23.html"]').selected = true;
+        break;
+    case 'SYR':
+        document.querySelector('option[value="index_24.html"]').selected = true;
+        break;
+    case 'SGP':
+        document.querySelector('option[value="index_25.html"]').selected = true;
+        break;
+    case 'ZWE':
+        document.querySelector('option[value="index_26.html"]').selected = true;
+        break;
+    case 'SDN':
+        document.querySelector('option[value="index_27.html"]').selected = true;
+        break;
+    case 'CHE':
+        document.querySelector('option[value="index_28.html"]').selected = true;
+        break;
+    case 'NOR':
+        document.querySelector('option[value="index_29.html"]').selected = true;
+        break;
+    case 'SWE':
+        document.querySelector('option[value="index_30.html"]').selected = true;
+        break;
+    case 'ESP':
+        document.querySelector('option[value="index_31.html"]').selected = true;
+        break;
+    case 'LKA':
+        document.querySelector('option[value="index_32.html"]').selected = true;
+        break;
+    case 'THA':
+        document.querySelector('option[value="index_33.html"]').selected = true;
+        break;
+    case 'KOR':
+        document.querySelector('option[value="index_34.html"]').selected = true;
+        break;
+    case 'TWN':
+        document.querySelector('option[value="index_35.html"]').selected = true;
+        break;
+    case 'CHN':
+        document.querySelector('option[value="index_36.html"]').selected = true;
+        break;
+    case 'CHL':
+        document.querySelector('option[value="index_37.html"]').selected = true;
+        break;
+    case 'DNK':
+        document.querySelector('option[value="index_38.html"]').selected = true;
+        break;
+    case 'DEU':
+        document.querySelector('option[value="index_39.html"]').selected = true;
+        break;
+    case 'TUR':
+        document.querySelector('option[value="index_40.html"]').selected = true;
+        break;
+    case 'NGA':
+        document.querySelector('option[value="index_41.html"]').selected = true;
+        break;
+    case 'NPL':
+        document.querySelector('option[value="index_42.html"]').selected = true;
+        break;
+    case 'PAK':
+        document.querySelector('option[value="index_43.html"]').selected = true;
+        break;
+    case 'PNG':
+        document.querySelector('option[value="index_44.html"]').selected = true;
+        break;
+    case 'HUN':
+        document.querySelector('option[value="index_45.html"]').selected = true;
+        break;
+    case 'BTN':
+        document.querySelector('option[value="index_46.html"]').selected = true;
+        break;
+    case 'PHL':
+        document.querySelector('option[value="index_47.html"]').selected = true;
+        break;
+    case 'FIN':
+        document.querySelector('option[value="index_48.html"]').selected = true;
+        break;
+    case 'BRA':
+        document.querySelector('option[value="index_49.html"]').selected = true;
+        break;
+    case 'FRA':
+        document.querySelector('option[value="index_50.html"]').selected = true;
+        break;
+    case 'BGR':
+        document.querySelector('option[value="index_51.html"]').selected = true;
+        break;
+    case 'VNM':
+        document.querySelector('option[value="index_52.html"]').selected = true;
+        break;
+    case 'VEN':
+        document.querySelector('option[value="index_53.html"]').selected = true;
+        break;
+    case 'PER':
+        document.querySelector('option[value="index_54.html"]').selected = true;
+        break;
+    case 'BEL':
+        document.querySelector('option[value="index_55.html"]').selected = true;
+        break;
+    case 'POL':
+        document.querySelector('option[value="index_56.html"]').selected = true;
+        break;
+    case 'HKG':
+        document.querySelector('option[value="index_57.html"]').selected = true;
+        break;
+    case 'MAC':
+        document.querySelector('option[value="index_58.html"]').selected = true;
+        break;
+    case 'MYS':
+        document.querySelector('option[value="index_59.html"]').selected = true;
+        break;
+    case 'ZAF':
+        document.querySelector('option[value="index_60.html"]').selected = true;
+        break;
+    case 'MMR':
+        document.querySelector('option[value="index_61.html"]').selected = true;
+        break;
+    case 'MEX':
+        document.querySelector('option[value="index_62.html"]').selected = true;
+        break;
+    case 'MNG':
+        document.querySelector('option[value="index_63.html"]').selected = true;
+        break;
+    case 'LAO':
+        document.querySelector('option[value="index_64.html"]').selected = true;
+        break;
+    case 'RUS':
+        document.querySelector('option[value="index_65.html"]').selected = true;
+        break;
+
+    default:
+        document.querySelector('option[value="index.html"]').selected = true;
+}
+
+document.getElementById('form').select.onchange = function () {
+    location.href = document.getElementById('form').select.value;
+}
